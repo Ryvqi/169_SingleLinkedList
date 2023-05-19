09031238de89fb85d148bd19fe92ad335e1a0547
Ryvqi <rifqikhabibi03@gmail.com>
2023-05-19 11:12:00 +0700

    node*

--- 169_SingleLinkedList/169_SingleLinkedList.cpp
+++ 169_SingleLinkedList/169_SingleLinkedList.cpp
@@ -31,4 +31,19 @@ void addNode() {
 		START = nodeBaru;
 		return;
 	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
 }
